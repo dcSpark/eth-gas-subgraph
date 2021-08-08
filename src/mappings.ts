3fc4319701dc5ac6cc788a2821b8bde242db76be
Nico Arqueros <nico.arquerosr@gmail.com>
2021-08-07 23:09:30 -0500

    add tx mapping

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,6 +1,7 @@
 import { ethereum } from '@graphprotocol/graph-ts'
 
-import { Block } from '../generated/schema'
+// @ts-ignore
+import { Block, Transaction } from '../generated/schema'
 
 export function handleBlock(block: ethereum.Block): void {
   let entity = new Block(block.number.toString())
@@ -12,4 +13,22 @@ export function handleBlock(block: ethereum.Block): void {
   entity.timestamp = block.timestamp
 
   entity.save()
+}
+
+export function handleTransaction(call: ethereum.Call): Transaction {
+    const tx = new Transaction(call.transaction.hash.toHex());
+    tx.blockNumber = call.block.number;
+    tx.blockHash = call.block.hash;
+    tx.timestamp = call.block.timestamp;
+
+    tx.hash = call.transaction.hash;
+    tx.index = call.transaction.index;
+    tx.from = call.transaction.from;
+    tx.to = call.transaction.to;
+    tx.value = call.transaction.value;
+    tx.gasUsed = call.transaction.gasUsed;
+    tx.gasPrice = call.transaction.gasPrice;
+    tx.input = call.transaction.input;
+
+    return tx as Transaction;
 }
